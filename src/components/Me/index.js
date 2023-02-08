8ec4ab989dd68c877ee790ba0fa807570c5764a9
Kazi Islam <kazichaska@gmail.com>
2023-02-07 21:45:05 -0600

    components and nav items created and tested

---
+++ src/components/Me/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Me = () => {
+    return (
+        <section>
+            <h1>About Me!</h1>
+            <p>This is the beginning of learning of React! which is awesome! 💁👌🎍😍 </p>
+        </section>
+    );
+}
+
+export default Me;
