44e73c5ab1569039f58d4a4e6ea0c032af619848
Kazi Islam <kazichaska@gmail.com>
2023-02-09 19:13:01 -0600

    created tests for about and nav

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,39 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+import React from "react";
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from "..";
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    // baseline test
+    test('renders', () => {
+        render(<Nav />);
+    })
+    // snapshot test
+    test('matches snapshot', () => {
+        const { asFragment } = render(<Nav />);
+        // assert value comparison
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
+
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+        const { getByLabelText } = render(<Nav />);
+
+        // eslint-disable-next-line testing-library/prefer-screen-queries
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+})
+
+describe('links are visible', () => {
+    it('inserts text into the links', () => {
+        // Arrange
+        const { getByTestId } = render(<Nav />);
+        // Assert
+        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+        expect(getByTestId('about')).toHaveTextContent('About me');
+    })
+})
